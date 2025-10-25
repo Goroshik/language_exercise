00bf8475ea5f81dea828997631e6cddc372d6483
p.gorohov <p.gorohov@smart-it.by>
2025-10-25 15:06:31 +0200

    Refactor AddWordModal to send words as an array in POST request; update validation check for words in route handler

--- src/services/wordsService.ts
+++ src/services/wordsService.ts
@@ -4,6 +4,7 @@ export async function searchWordsService(userId: string, query: string) {
   return wordRepository.searchWords(userId, query);
 }
 
+// TODO: need to fix any type
 export async function addManyWordService(userId: string, words: any[]) {
   return wordRepository.addManyWord(userId, words);
 }
